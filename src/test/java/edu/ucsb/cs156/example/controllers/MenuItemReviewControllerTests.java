cca7b52a461df376eaf9670b73cd7750e445922d
Logan Melgoza <loganmelgoza@ucsb.edu>
2025-10-28 14:03:08 -0700

    lm - added GET endpoint for MenuItemReview

--- src/test/java/edu/ucsb/cs156/example/controllers/MenuItemReviewControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/MenuItemReviewControllerTests.java
@@ -18,6 +18,8 @@ import edu.ucsb.cs156.example.testconfig.TestConfig;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -47,6 +49,13 @@ public class MenuItemReviewControllerTests extends ControllerTestCase {
     mockMvc.perform(get("/api/menuitemreview/all")).andExpect(status().is(200)); // logged
   }
 
+  @Test
+  public void logged_out_users_cannot_get_by_id() throws Exception {
+    mockMvc
+        .perform(get("/api/menuitemreview?id=123"))
+        .andExpect(status().is(403)); // logged out users can't get by id
+  }
+
   // Authorization tests for /api/menuitemreview/post
   // (Perhaps should also have these for put and delete)
   @Test
@@ -63,6 +72,59 @@ public class MenuItemReviewControllerTests extends ControllerTestCase {
   }
 
   // // Tests with mocks for database actions
+  @WithMockUser(roles = {"USER"})
+  @Test
+  public void test_that_logged_in_user_can_get_by_id_when_the_id_exists() throws Exception {
+
+    // arrange
+    LocalDateTime ldt = LocalDateTime.parse("2022-01-03T00:00:00");
+
+    MenuItemReview menuItemReview =
+        MenuItemReview.builder()
+            .itemId(1)
+            .reviewerEmail("testEmail1@gmail.com")
+            .stars(4)
+            .dateReviewed(ldt)
+            .comments("Not Bad")
+            .build();
+
+    when(menuItemReviewRepository.findById(eq(123L))).thenReturn(Optional.of(menuItemReview));
+
+    // act
+    MvcResult response =
+        mockMvc.perform(get("/api/menuitemreview?id=123")).andExpect(status().isOk()).andReturn();
+
+    // assert
+
+    verify(menuItemReviewRepository, times(1)).findById(eq(123L));
+    String expectedJson = mapper.writeValueAsString(menuItemReview);
+    String responseString = response.getResponse().getContentAsString();
+    assertEquals(expectedJson, responseString);
+  }
+
+  @WithMockUser(roles = {"USER"})
+  @Test
+  public void test_that_logged_in_user_can_get_by_id_when_the_id_does_not_exist() throws Exception {
+
+    // arrange
+
+    when(menuItemReviewRepository.findById(eq(123L))).thenReturn(Optional.empty());
+
+    // act
+    MvcResult response =
+        mockMvc
+            .perform(get("/api/menuitemreview?id=123"))
+            .andExpect(status().isNotFound())
+            .andReturn();
+
+    // assert
+
+    verify(menuItemReviewRepository, times(1)).findById(eq(123L));
+    Map<String, Object> json = responseToJson(response);
+    assertEquals("EntityNotFoundException", json.get("type"));
+    assertEquals("MenuItemReview with id 123 not found", json.get("message"));
+  }
+
   @WithMockUser(roles = {"USER"})
   @Test
   public void logged_in_user_can_get_all_menuitemreview() throws Exception {
@@ -109,7 +171,7 @@ public class MenuItemReviewControllerTests extends ControllerTestCase {
 
   @WithMockUser(roles = {"ADMIN", "USER"})
   @Test
-  public void an_admin_user_can_post_a_new_ucsbdate() throws Exception {
+  public void an_admin_user_can_post_a_new_menuitemreview() throws Exception {
     // arrange
 
     LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");

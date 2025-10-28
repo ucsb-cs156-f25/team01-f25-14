e62c3be48ffe9cf1960894c7cdb737fe7b9183ac
Logan Melgoza <loganmelgoza@ucsb.edu>
2025-10-28 01:34:02 -0700

    lm - created MenuItemReviewController

---
+++ src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewController.java
@@ -0,0 +1,81 @@
+package edu.ucsb.cs156.example.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import edu.ucsb.cs156.example.entities.MenuItemReview;
+import edu.ucsb.cs156.example.repositories.MenuItemReviewRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/** This is a REST controller for MenuItemReview */
+@Tag(name = "MenuItemReview")
+@RequestMapping("/api/menuitemreview")
+@RestController
+@Slf4j
+public class MenuItemReviewController extends ApiController {
+
+  @Autowired MenuItemReviewRepository menuItemReviewRepository;
+
+  /**
+   * List all Menu Item Reviews
+   *
+   * @return an iterable of MenuItemReview
+   */
+  @Operation(summary = "List all menu item review")
+  @PreAuthorize("hasRole('ROLE_USER')")
+  @GetMapping("/all")
+  public Iterable<MenuItemReview> allMenuItemReview() {
+    Iterable<MenuItemReview> review = menuItemReviewRepository.findAll();
+    return review;
+  }
+
+  /**
+   * Create a menu item review
+   *
+   * @param itemId the menu item ID
+   * @param reviewerEmail the menu item ID
+   * @param stars the number of stars given
+   * @param dateReviewed the date
+   * @param comments the date
+   * @return the saved menuitemreview
+   */
+  @Operation(summary = "Create a menuItemReview")
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  @PostMapping("/post")
+  public MenuItemReview postMenuItemReview(
+      @Parameter(name = "itemId") @RequestParam long itemId,
+      @Parameter(name = "reviewerEmail") @RequestParam String reviewerEmail,
+      @Parameter(name = "stars") @RequestParam int stars,
+      @Parameter(
+              name = "dateReviewed",
+              description = "date-time (ISO 8601, e.g. 2022-04-20T00:00:00)")
+          @RequestParam("dateReviewed")
+          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+          LocalDateTime dateReviewed,
+      @Parameter(name = "comments") @RequestParam String comments)
+      throws JsonProcessingException {
+
+    log.info("dateReviewed={}", dateReviewed);
+
+    MenuItemReview menuItemReview = new MenuItemReview();
+    menuItemReview.setItemId(itemId);
+    menuItemReview.setReviewerEmail(reviewerEmail);
+    menuItemReview.setStars(stars);
+    menuItemReview.setDateReviewed(dateReviewed);
+    menuItemReview.setComments(comments);
+
+    MenuItemReview savedMenuItemReview = menuItemReviewRepository.save(menuItemReview);
+
+    return savedMenuItemReview;
+  }
+}

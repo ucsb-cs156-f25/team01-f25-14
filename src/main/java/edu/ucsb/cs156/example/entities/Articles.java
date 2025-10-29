5054b25de191b5bafe9ecc51618a8818dd3b4252
keyizong27 <keyizong@ucsb.edu>
2025-10-28 18:18:29 -0700

    kz - changed entity file

--- src/main/java/edu/ucsb/cs156/example/entities/Articles.java
+++ src/main/java/edu/ucsb/cs156/example/entities/Articles.java
@@ -1,94 +1,32 @@
-package com.example.demo.entity;
+package edu.ucsb.cs156.example.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "articles")
-public class Articles {
+/**
+ * This is a JPA entity that represents a MenuItemReview, i.e. a review that is attached to a
+ * specific menu item
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "menuitemreview")
+public class MenuItemReview {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)  // 主键自增
-    private long id;
-
-    private String title;
-
-    private String url;
-
-    private String explanation;
-
-    private String email;
-
-    private LocalDateTime dateAdded;
-
-    public Articles() {
-    }
-
-    public Articles(String title, String url, String explanation, String email, LocalDateTime dateAdded) {
-        this.title = title;
-        this.url = url;
-        this.explanation = explanation;
-        this.email = email;
-        this.dateAdded = dateAdded;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getExplanation() {
-        return explanation;
-    }
-
-    public void setExplanation(String explanation) {
-        this.explanation = explanation;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDateTime getDateAdded() {
-        return dateAdded;
-    }
-
-    public void setDateAdded(LocalDateTime dateAdded) {
-        this.dateAdded = dateAdded;
-    }
-
-    @Override
-    public String toString() {
-        return "Articles{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", url='" + url + '\'' +
-                ", explanation='" + explanation + '\'' +
-                ", email='" + email + '\'' +
-                ", dateAdded=" + dateAdded +
-                '}';
-    }
+  private String title;
+  private String url;
+  private String explanation;
+  private String email;
+  private LocalDateTime dateAdded;
 }

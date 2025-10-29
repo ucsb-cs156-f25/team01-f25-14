bde9d5f8a85b73423fd98d96b0972c1bc7840789
Pratima <p_nallapareddy@ucsb.edu>
2025-10-28 17:42:10 -0700

    deleted articles

--- src/main/java/edu/ucsb/cs156/example/repositories/ArticlesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Articles;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticlesRepository extends JpaRepository<Articles, Long> {}
